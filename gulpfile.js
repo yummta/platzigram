47f6325dbdac2e09244ad0e09909b68286173009
yummta <yummta@gmail.com>
2016-11-21 07:51:23 -0500

    sirviendo archivos estaticos e implementación de ES6 con browserify y babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
+var babel = require('babelify');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 
 gulp.task('styles', function(){
 	gulp
@@ -11,4 +14,19 @@ gulp.task('styles', function(){
 
 })
 
-gulp.task('default', ['styles'])
+gulp.task('assets', function(){
+	gulp
+	.src('assets/*')
+	.pipe(gulp.dest('public'));
+})
+
+gulp.task('scripts', function(){
+	browserify('src/index.js')
+	.transform(babel)
+	.bundle()
+	.pipe(source('index.js'))
+	.pipe(rename('app.js'))
+	.pipe(gulp.dest('public'));
+})
+
+gulp.task('default', ['styles', 'assets', 'scripts'])
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'pug');
+
+app.use(express.static('public'));
+
 app.get('/', function(req, res){
-	res.send('hello world')
+	res.render('index');
 });
 
 app.listen(3000, function(err){
 	if (err) return console.log("Hubo un error"), process.exit(1);
 	console.log('Platzigram escuchando en el puerto 3000')
- })
+})
